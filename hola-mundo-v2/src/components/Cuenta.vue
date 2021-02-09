ab3c790201be1de510596bb47e172731754fa29f
Mike <lanselot777hailander@gmail.com>
2021-02-09 16:31:28 -0400

    Some changes

--- hola-mundo-v2/src/components/Cuenta.vue
+++ hola-mundo-v2/src/components/Cuenta.vue
@@ -5,16 +5,46 @@
 	<div v-for="(servicio, index) in servicios" :key="index">
 		{{ index+1 }} - {{ servicio }}
 	</div>
+	<!--El @ permite mandar funciones al componente hijo-->
+	<AccionSaldo 
+		texto="Aumentar Saldo"		
+		@accion="aumentar"		
+	/>
+	<AccionSaldo 
+		texto="Disminuir Saldo"
+		@accion="disminuir"
+		:desactivar="desactivar"
+	/>
 </template>
 
 <script>
-export default {
+import AccionSaldo from './AccionSaldo'
+
+export default {	
 	data (){
 		return {
 			saldo: 1000,
 			cuenta: 'Visa',
 			estado: true,
-			servicios: ['giro', 'abono', 'transferencia']
+			servicios: ['giro', 'abono', 'transferencia'],
+			desactivar: false
+		}
+	},
+	components:{
+		AccionSaldo
+	},
+	methods: {
+		aumentar(){
+			this.saldo = this.saldo + 100
+			this.desactivar = false
+		},
+		disminuir(){
+			if(this.saldo == 0){
+				this.desactivar = true
+				alert('Saldo Agotado')
+				return
+			}
+			this.saldo = this.saldo - 100
 		}
 	}
 }
