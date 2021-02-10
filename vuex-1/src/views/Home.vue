57a49651977e53a3f7ee2478475b258a1941f778
Mike <lanselot777hailander@gmail.com>
2021-02-10 11:07:50 -0400

    Vuex

---
+++ vuex-1/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">    
+    <!--<h1>Mi contador: {{ $store.state.contador }}</h1>-->
+    <h1 :style="colorContador">{{titulo}}: {{ contador }}</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import {mapState} from 'vuex'
+
+export default {
+  name: 'Home',
+  components: {
+    
+  },
+  data(){
+  	return {
+  		titulo: 'Mi contador Vuex'
+  	}
+  },
+  computed: {
+  	...mapState(['contador']),
+  	colorContador(){
+  		return [this.contador > 100 ? {'color':'green'} : {'color': 'red'}]
+  	}
+  }
+}
+</script>
