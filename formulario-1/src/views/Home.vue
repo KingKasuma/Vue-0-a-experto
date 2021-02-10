a922c0f8de9b69de972d05ffbd5d282faaa38a00
Mike <lanselot777hailander@gmail.com>
2021-02-10 15:35:48 -0400

    Some changes

--- formulario-1/src/views/Home.vue
+++ formulario-1/src/views/Home.vue
@@ -1,7 +1,75 @@
 <template>
-  <div class="home">
-    inicio
-  </div>
+  <form @submit.prevent="procesarFormulario">
+  	  <input 
+  	  	type="text" name="" 
+  	  	class="form-control my-w" 
+  	  	placeholder="Ingrese un nombre" 
+  	  	v-model.trim="tarea.nombre"
+  	  >
+
+  	  <div class="form-check form-check-inline">
+  	  	<input
+  	  		type="checkbox"
+  	  		id="check-1"
+  	  		class="form-check-input"
+  	  		v-model="tarea.categorias"
+  	  		value="javascript"
+  	  	>
+  	  	<label for="check-1" class="form-check-label">Javascript</label>
+  	  </div>
+  	  <div class="form-check form-check-inline">
+  	  	<input
+  	  		type="checkbox"
+  	  		id="check-2"
+  	  		class="form-check-input"
+  	  		v-model="tarea.categorias"
+  	  		value="node js"
+  	  	>
+  	  	<label for="check-2" class="form-check-label">Node.js</label>
+  	  </div>
+
+  	  <div class="mt-2">
+  	  	<div class="form-check form-check-inline">
+  	  		<input 
+  	  			type="radio" 
+  	  			id="radio-1"
+  	  			class="form-check-input"
+  	  			value="urgente"
+  	  			v-model="tarea.estado"
+  	  		>
+  	  		<label for="radio-1" class="form-check-label">Urgente</label>
+  	  	</div>
+  	  	<div class="form-check form-check-inline">
+  	  		<input 
+  	  			type="radio" 
+  	  			id="radio-2"
+  	  			class="form-check-input"
+  	  			value="relax"
+  	  			v-model="tarea.estado"
+  	  		>
+  	  		<label for="radio-2" class="form-check-label">Relax</label>
+  	  	</div>
+  	  </div>  	
+
+  	  <div class="mt-2">
+  	  	<input 
+  	  		type="number"
+  	  		class="form-control" 
+  	  		v-model.number="tarea.numero"
+  	  	>
+  	  </div>
+
+  	  <button 
+  	  	class="btn btn-dark mt-2 btn-block" 
+  	  	type="submit"
+  	  	:disabled="bloquear"
+  	  >
+  		Procesar
+  	  </button>
+
+  </form>
+  <hr>
+  <p>{{tarea}}</p>
 </template>
 
 <script>
@@ -10,6 +78,39 @@ export default {
   name: 'Home',
   components: {
     
+  },
+  data(){
+  	return {
+  		tarea: {
+  			nombre: '',
+  			categorias: [],
+  			estado: '',
+  			numero: 0
+  		}
+  	}
+  },
+  computed:{
+  	bloquear(){
+  		return this.tarea.nombre.trim() === "" ? true : false
+  	}
+  },
+  methods: {
+  	procesarFormulario(){
+  		console.log(this.tarea)
+  		if(this.tarea.nombre.trim() === ""){
+  			console.log('Campo vacio')
+  			return
+  		}
+  		console.log('no esta vacio')
+  		// envian los datos
+
+  		this.tarea = {
+  			nombre: '',
+  			categorias: [],
+  			estado: '',
+  			numero: 0
+  		}
+  	}
   }
 }
 </script>
